68dd5885c005c245f5ae8ecd63c20115b0d21cb8
luchinileo <leonardoluchini@tdf.edu.ar>
2022-09-29 21:25:51 -0300

    cantidadDeVueltas

---
+++ app.ts
@@ -0,0 +1,15 @@
+let readlineSync = require('readline-sync');
+
+
+const VUELTAS = 4 //cantidad de vueltas
+let total = 0;
+let promedio = 0;
+for(let i = 0; i < VUELTAS; i++){
+    let vuelta = readlineSync.question("Indique el tiempo de la vuelta: ");
+    //el valor vuelta almacena string por lo cual lo convertimos a un numero con parseInt()
+    total +=  parseInt(vuelta )
+}
+
+promedio = total/VUELTAS
+console.log("El total de las vueltas es : " + total)
+console.log("El promedio de las vueltas es : " + promedio)
